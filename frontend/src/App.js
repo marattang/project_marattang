d814d385e680995dc381417a8d78cd2214147a49
marattang <tlawlfp0322@gmail.com>
2021-06-03 17:52:07 +0900

    2021-06-03-commit-02

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,16 @@
 import React from 'react'
+import { BrowserRouter as Router, Link } from "react-router-dom"
+import styled from 'styled-components';
 
 const App = () => {
-  return (<>
+  return (<Container>
+    <Element>
     <nav>
-      <Link to='.index.html'>home</Link>
-      <Link to="./blog_list.html">blog</Link>
-      <Link to="./about_me.html">about me</Link>
+      <Router>
+        <Link to='/'>home</Link>
+        <Link to='/'>blog</Link>
+        <Link to='/'>about me</Link>
+      </Router>
     </nav>
 
     <h1>첫번째 크기 헤드라인</h1>
@@ -14,11 +19,17 @@ const App = () => {
     <h4>네번째 크기 헤드라인</h4>
     <h5>다섯번째 크기 헤드라인</h5>
     <p>문단은 p로 쓰세요. p는 아마도 Paragraph의 앞글자를 따온 것이겠죠?</p>
-    <Link to="https://www.google.com">Go to google</Link>
     <hr/>
     <img sre="./images/stay_funky.jpg" width="600px"/>
-
-  </>)
+  </Element>
+  </Container>)
 }
-
+const Container = styled.div`
+    width: 100%,
+    border: 1px solid #d1d8e4;
+s`
+const Element = styled.div`
+    width: 400px;
+    margin: 0 auto;
+`
 export default App
